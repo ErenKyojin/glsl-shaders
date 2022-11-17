30122a5d8e0a8f2563277420219c62f61936f7e3
DESKTOP-R8DKP0L\monte <francesco.montefusco@live.it>
2022-11-17 19:30:04 +0100

    updated filename, added exponential

--- bookOfShaders/01_line.fs
+++ bookOfShaders/01_line.fs
@@ -1,3 +1,8 @@
+/*From the book of shaders: https://thebookofshaders.com/05/
+**
+**
+*/
+
 #ifdef GL_ES
 precision mediump float;
 #endif
@@ -8,6 +13,7 @@ uniform float u_time;
 
 // Plot a line on Y using a value between 0.0-1.0
 float plot(vec2 st) {    
+    //returns about 1.0 when abs(st.y-st.x) <= 0.02 (making it smooth)
     return smoothstep(0.02, 0.0, abs(st.y - st.x));
 }
 
@@ -15,11 +21,13 @@ void main() {
 	vec2 st = gl_FragCoord.xy/u_resolution;
 
     float y = st.x;
+    //mouse coordinates normalized from 0.0 to 1.0
     vec2 normalizedMouse = u_mouse/u_resolution;
 
+    //plots the background, with green value changing based on the mouse position on the y axis
     vec3 color = vec3(y, normalizedMouse.y, 0.0);
 
-    // Plot a line
+    // Plot a line, red changes based on the mouse position on the x axis
     float pct = plot(st);
     color = (1.0-pct)*color+pct*vec3(normalizedMouse.x, 0.8667, 0.0);
 
